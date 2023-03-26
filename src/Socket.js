1fcd89c1e8cb5e6b963da2e8468711f43a32adcd
Hugo Mellouet <hugo.mellouet@gmail.com>
2023-03-26 14:43:02 +0200

    modif css

--- src/Socket.js
+++ src/Socket.js
@@ -1,9 +1,9 @@
 import io from "socket.io-client";
 
-/*export const socket = io.connect("https://wordliar.adaptable.app", {
-  transports: ["polling"]
-});*/
-
-export const socket = io.connect("http://localhost:3001", {
+export const socket = io.connect("https://wordliar.adaptable.app", {
   transports: ["polling"]
 });
+
+/*export const socket = io.connect("http://localhost:3001", {
+  transports: ["polling"]
+});*/
